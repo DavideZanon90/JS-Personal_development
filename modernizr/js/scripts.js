a75bd083857277eb68374cc5fa65916b8bb43d58
Davide Zanon <davidezanon@wlnss.com>
2015-11-12 16:15:01 +0100

    Images and scripts added

---
+++ modernizr/js/scripts.js
@@ -0,0 +1,29 @@
+if (Modernizr.opacity) {
+	console.log('opdacity is supported');
+}
+
+if (Modernizr.touch) {
+	console.log('touch is supported');
+} else {
+	console.log('touch is not supported');
+}
+
+// Test if it's night or not
+Modernizr.addTest('night', function() {
+	var current = new Date();
+	current = current.getHours();
+	console.log(!!(current >= 19));
+});
+
+// Extension to use different image formats according to the use
+var path = document.getElementById('star-svg').getAttribute('data-path');
+var	svgObj;
+
+if (Modernizr.svg) {
+	svgObj.innerHtml = '<object type="img/svg+xml" style="float:right" data="' 
+								+ path + '.png" ></obect>'
+} else {
+	svgObj.innerHtml = '<img src="' + path + '.png" />';
+}
+
+document.getElementById('star-svg').innerHtml = svgObj;
