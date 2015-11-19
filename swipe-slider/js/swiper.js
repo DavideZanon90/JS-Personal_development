eec3297a2e34444ff88c33155de636a94b4af3fb
Davide Zanon <davidezanon@wlnss.com>
2015-11-19 09:38:14 +0100

    Slider added

---
+++ swipe-slider/js/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  slidesPerView: 1,
+  centeredSlides: true,
+  paginationClickable: true,
+  spaceBetween: 30,
+  speed: 1000
+});
